2618126a0cc6536e1155a5f4c77912f9c970f195
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-03-31 23:14:08 +0300

    chore: update package.nix for pi-mono integration

--- package.nix
+++ package.nix
@@ -1,32 +1,42 @@
 {
   pkgs,
   version ? "0.1.0",
+  hash ? null,
+  npmDepsHash ? null,
 }:
 let
-  src = ./.;
-in
-pkgs.stdenv.mkDerivation {
-  pname = "pi-worktree";
-  inherit version src;
+  srcArg =
+    if hash != null then
+      pkgs.fetchFromGitHub {
+        owner = "kesor";
+        repo = "pi-worktree";
+        rev = "v${version}";
+        inherit hash;
+      }
+    else
+      ./.;
 
-  buildPhase = ''
-    # Check if dist exists, if not we can't build in sandbox
-    if [ ! -d "dist" ]; then
-      echo "ERROR: dist/ folder not found. Run 'npm run build' first."
-      exit 1
-    fi
-    echo "dist/ found, packaging..."
-  '';
+  npmDepsArg = if npmDepsHash != null then
+    pkgs.fetchNpmDeps {
+      src = srcArg;
+      hash = npmDepsHash;
+    }
+  else
+    null;
+in
+pkgs.buildNpmPackage {
+  pname = "pi-worktree";
+  inherit srcArg version;
+
+  src = srcArg;
+  npmDeps = npmDepsArg;
+
+  buildInputs = [ pkgs.nodejs_22 ];
 
   installPhase = ''
-    mkdir -p $out/lib/node_modules/pi-worktree
-    cp -r dist README.md LICENSE package.json $out/lib/node_modules/pi-worktree/
+    runHook preInstall
+    mkdir -p $out/lib/node_modules
+    cp -r . $out/lib/node_modules/pi-worktree
+    runHook postInstall
   '';
-
-  meta = with pkgs.lib; {
-    description = "Git worktree sandboxes for safe experimentation";
-    homepage = "https://github.com/kesor/pi-worktree";
-    license = licenses.mit;
-    platforms = platforms.all;
-  };
 }
